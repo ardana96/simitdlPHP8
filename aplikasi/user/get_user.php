387d554c1ab5a764b506442ca4724e1efe09c492
UrMama <141613642+Mudipas@users.noreply.github.com>
2025-01-17 17:38:02 +0700

    UserMenupart2

--- aplikasi/user/get_user.php
+++ aplikasi/user/get_user.php
@@ -1,38 +1,3 @@
-#region old code
-<?php
-// Konfigurasi koneksi MySQL
-// $server = "localhost";
-// $username = "root";
-// $password = "dlris30g";
-// $database = "sitag";
-
-// // Membuka koneksi
-// $connection = mysql_connect($server, $username, $password);
-// mysql_select_db($database, $connection);
-
-//include('../../config.php');
-
-// $connection = $koneksi;
-
-//$id = $_GET['id'];
-
-// Query untuk mendapatkan data perangkat utama
-// $query = "SELECT * FROM tuser WHERE id_user = '$id'";
-// $result = mysql_query($query);
-// $user = mysql_fetch_assoc($result);
-
-// Gabungkan data perangkat dan item dalam satu array
-// $response = array (
-//     'user' => $user
-// );
-
-// Ubah array menjadi format JSON dan kirim sebagai respons
-// echo json_encode($response);
-
-// Tutup koneksi
-// mysql_close($connection);
-?>
-#endregion
 <?php
 include('../../config.php'); // Pastikan config.php menggunakan sqlsrv_connect()
 
@@ -76,3 +41,39 @@ echo json_encode($response);
 // **Tutup koneksi**
 sqlsrv_close($conn);
 ?>
+
+#region old code
+<?php
+// Konfigurasi koneksi MySQL
+// $server = "localhost";
+// $username = "root";
+// $password = "dlris30g";
+// $database = "sitag";
+
+// // Membuka koneksi
+// $connection = mysql_connect($server, $username, $password);
+// mysql_select_db($database, $connection);
+
+//include('../../config.php');
+
+// $connection = $koneksi;
+
+//$id = $_GET['id'];
+
+// Query untuk mendapatkan data perangkat utama
+// $query = "SELECT * FROM tuser WHERE id_user = '$id'";
+// $result = mysql_query($query);
+// $user = mysql_fetch_assoc($result);
+
+// Gabungkan data perangkat dan item dalam satu array
+// $response = array (
+//     'user' => $user
+// );
+
+// Ubah array menjadi format JSON dan kirim sebagai respons
+// echo json_encode($response);
+
+// Tutup koneksi
+// mysql_close($connection);
+?>
+#endregion
