670c686795bfe49e9676980e8790bee6654bc006
UrMama <141613642+Mudipas@users.noreply.github.com>
2025-03-03 14:41:00 +0700

    pagination bug fix

--- aplikasi/stockopname/scriptstop/update_scriptop.php
+++ aplikasi/stockopname/scriptstop/update_scriptop.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 header('Content-Type: application/javascript');
 ?>
 
@@ -15,4 +15,23 @@ $(document).ready(function() {
 
     // Fokus ke input 'from' saat halaman dimuat
     $('#from').focus();
+}); -->
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+// Pastikan jQuery sudah dimuat sebelum kode ini dijalankan
+function dontEnter(evt) { 
+    evt = evt || window.event;
+    var node = evt.target || evt.srcElement;
+    if (evt.keyCode == 13 && node.type == "text") return false;
+}
+
+$(document).ready(function() {
+    // Bind fungsi dontEnter ke event keypress
+    $(document).on('keypress', dontEnter);
+
+    // Fokus ke input 'from' saat halaman dimuat
+    $('#from').focus();
 });
+
+</script>
